50f611bb6966694eeac0caa9da675f25b9ff1769
AlmeidaFabioO <fabio.633@hotmail.com>
2020-12-18 15:26:08 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,86 @@
+const cards = document.querySelectorAll('.card');
+const game = document.querySelector('.memory-game');
+const win = document.querySelector('#win');
+
+let hasFlippedCard = false;
+let firstCard, secondCard;
+let lockBoard = false;
+let match = 0;
+
+//função para virar carta
+function flipCard() {
+    if(lockBoard) return;
+    if(this === firstCard) return;
+
+    this.classList.add('flip');
+    if(!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    hasFlippedCard = false;
+    checkForMatch();
+}
+
+//função que checa se as cartas são iguais
+function checkForMatch() {
+    if(firstCard.dataset.card === secondCard.dataset.card) {
+        match = match + 1;
+        disableCards();
+
+        if(match == 6) {
+            game.style.display = "none";
+            win.style.display = "block";
+        
+            setTimeout(refreshGame, 4000);
+        }
+        return;
+    }
+
+    unflipCards();
+}
+
+function refreshGame() {
+    location.reload();
+}
+
+//função que desabilita as cartas
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
+}
+
+//funcão que desvira as cartas
+function unflipCards() {
+    lockBoard = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        resetBoard();
+    }, 1000);
+}
+
+//função que reseta o tabuleiro
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+//função que embaralha as cartas
+(function shuffle() {
+    cards.forEach((card) => {
+        let ramdomPosition = Math.floor(Math.random() * 12);
+        card.style.order = ramdomPosition;
+    })
+})();
+
+//adiciona evento de clique na carta
+cards.forEach((card) => {
+    card.addEventListener('click', flipCard)
+});
